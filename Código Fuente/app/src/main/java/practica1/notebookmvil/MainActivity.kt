dde516a1788f1c3440c431db8fb0a8b731173045
MichaelMarinReyes <michaelmarin201831260@cunoc.edu.gt>
2025-02-13 21:27:21 -0600

    Creación de proyecto en Android Studio y carpeta de Documentación

---
+++ Código Fuente/app/src/main/java/practica1/notebookmvil/MainActivity.kt
@@ -0,0 +1,47 @@
+package practica1.notebookmvil
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import practica1.notebookmvil.ui.theme.NotebookMóvilTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NotebookMóvilTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NotebookMóvilTheme {
+        Greeting("Android")
+    }
+}
